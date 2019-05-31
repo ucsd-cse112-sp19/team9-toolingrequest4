0b2ab227bef33ac48af9938a2d7e94e04443be51
ahaggart <alex.haggart@gmail.com>
2019-05-31 13:08:59 -0700

    minimum viable product

--- src/options.js
+++ src/options.js
@@ -14,20 +14,46 @@ function constructOptions(kButtonColors) {
 }
 constructOptions(kButtonColors);
 
-chrome.storage.sync.get('repo', function (data) {
-  let repo = data['repo'];
-  let repo_input = document.getElementById('repo');
-  if( repo ) {
-    repo_input.value = ''+repo;
+chrome.storage.sync.get(['repo', 'owner'], function (data) {
+  let repo = document.getElementById('repo');
+  let owner = document.getElementById('owner');
+  
+  let user = document.getElementById('user');
+  let pass = document.getElementById('pass');
+  if( data['repo'] ) {
+    repo.value = ''+data['repo'];
   } else {
-    repo_input.placeholder = 'repository url';
+    repo.placeholder = 'repository name';
+  }
+
+  if( data['owner'] ) {
+    repo.value = ''+data['owner'];
+  } else {
+    repo.placeholder = 'repository owner';
+  }
+
+  function authenticate(user, pass) {
+    console.log('attempting authentication');
+    let token = user + ":" + pass;
+    let hash = btoa(token);
+    let req = new XMLHttpRequest();
+    req.open("GET", "https://api.github.com/authorizations", true, user, pass);
+    req.setRequestHeader('Authorization', 'Basic ' + hash);
+    req.onload = () => {
+      console.log(req.responseText);
+    };
+    req.send();
   }
 
   function updateRepositoryInfo(e) {
     e.preventDefault();
-    if( repo_input.value ) {
-      chrome.storage.sync.set({'repo': repo_input.value});
+    if( repo.value ) {
+      chrome.storage.sync.set({'repo': repo.value});
     }
+    if( owner.value ) {
+      chrome.storage.sync.set({'owner': owner.value});
+    }
+    // authenticate(user.value, pass.value);
   }
 
   document.getElementById("settings").onsubmit = updateRepositoryInfo;
--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,77 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+// changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+//   };
+
+function makeRow(text) {
+  let elem = document.createElement('div');
+  elem.innerText = text;
+  return elem;
+}
+
+function appendRow(parent, text) {
+  parent.appendChild(makeRow(text));
+}
+
+function addContribs(contribs, data) {
+
+  data.forEach((user)=>{
+    let panel = document.createElement('div');
+    panel.style.borderColor = "blue";
+    panel.style.borderWidth = "2px";
+    panel.style.borderStyle = "solid";
+    panel.style.borderRadius = "2px";
+    panel.style.margin = "2px";
+
+    let append = (text) => appendRow(panel, text);
+    append("Author: " + user["author"]["login"]);
+    weeks = user["weeks"];
+    append("Past Week: " + weeks[weeks.length - 1]["a"]);
+    append("Total Contributions: " + user["total"]);
+
+    contribs.appendChild(panel);
+  });
+}
+
+function displayInfo(info) {
+  let contribs = document.getElementById("contribs");
+  addContribs(contribs, JSON.parse(info));
+}
+
+function getContributions(repo, owner) {
+  let req = new XMLHttpRequest();
+  let url = "https://api.github.com/repos/" + owner + "/" + repo + "/stats/contributors";
+  req.open("GET", url, true);
+  req.onload = () => {
+    displayInfo(req.responseText);
   };
+  req.send();
+}
+
+let repo_key = "repo", owner_key = "owner";
+
+chrome.storage.sync.get([repo_key, owner_key], function(data) {
+  let repo = data[repo_key];
+  let owner = data[owner_key];
+  if( !(repo && owner) ) {
+    displayInfo("not enough info");
+    return;
+  }
+
+  document.getElementById('repo_name').value = repo;
+  document.getElementById('repo_owner').value = owner;
+
+  getContributions(repo, owner);
+})
+
